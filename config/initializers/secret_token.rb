f88ede5a95b5ab4f159943ae690829a4d17fafab
hzmangel <hzmangel@gmail.com>
2013-11-30 23:29:11 +0800

    Init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Base64ImageCarrierwave::Application.config.secret_key_base = '97682edac98768b489e2701e7a32f12732645fcb8a1ff6bd1508dd6283c8510d49083cdad1e74321efcfc6926d15a23cefc570df1cd54e67a4af1c9ad740a3d5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Base64ImageCarrierwave::Application.config.session_store :cookie_store, key: '_base64_image_carrierwave_session'
