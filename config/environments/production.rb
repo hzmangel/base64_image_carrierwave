7d0ec9f4c75d9794ed96c2f49c9bdded366640dc
hzmangel <hzmangel@gmail.com>
2013-12-01 08:45:22 +0800

    Add fonts to assets pipeline

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,12 @@ Base64ImageCarrierwave::Application.configure do
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
 
+  # Add the fonts path
+  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+
+  # Precompile additional assets
+  config.assets.precompile += %w( .svg .eot .woff .ttf msite.js msite.css )
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
